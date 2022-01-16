5ebc613aa7c7dd37aced4f43d40c5a709fbe1b46
jaykhans <jahongirnagmatov@gmail.com>
2022-01-16 21:34:10 +0500

    update cipher task

--- Day 8/ceaser-cipher.py
+++ Day 8/ceaser-cipher.py
@@ -1,39 +1,49 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'e' to encrypt, type 'd' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-
-
-
-
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-def ceaser(direction_input, text_input, shift_input):
-
- 
-
-  def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
+def encode(text, shift):
+  encoded_text = ""
+  for letter in text:
+    if letter in alphabet:
       position = alphabet.index(letter)
-      new_position = position + shift_amount
-      cipher_text += alphabet[new_position]
-    print(f"The encoded text is: \n{cipher_text}")
+      new_position = position + shift
+      if new_position > len(alphabet) - 1:
+        new_position -= len(alphabet) - 1
+      encoded_text += alphabet[new_position]      
+    elif letter == " ":
+      encoded_text += " "
+  if letter not in alphabet:
+    print("You've typed not only letters.")
+    
+  else :
+    print(encoded_text)
 
-  def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
+def decode(text, shift):
+  decoded_text = ""
+  for letter in text:
+    if letter in alphabet:
       position = alphabet.index(letter)
-      new_position = position - shift_amount
-      plain_text += alphabet[new_position]
-    print(f"The decoded text is: \n{plain_text}")
+      new_position = position - shift
+      if new_position < 0:
+        new_position += len(alphabet) - 1
+      decoded_text += alphabet[new_position]
+    elif letter == " ":
+      decoded_text += " "
+  if letter not in alphabet:
+    print("You've typed not only letters.")
+    
+  print(decoded_text)
 
-  if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-  elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
+def main(direction):
+  if direction == "e":
+    encode(text, shift)
+  elif direction== "d":
+    decode(text, shift)
+  else :
+    print("Yo've typed invalid command.")
 
-ceaser(direction_input = direction, text_input = text, shift_input = shift)
+main(direction)
