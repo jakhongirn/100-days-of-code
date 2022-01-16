b7375849473b5f002fbeb6942214b93fe27f7ad9
jaykhans <jahongirnagmatov@gmail.com>
2022-01-16 17:17:44 +0500

    update prime number checker

--- Day 8/prime-number-checker.py
+++ Day 8/prime-number-checker.py
@@ -1,24 +1,37 @@
+#Write your code below this line 👇
 import math
-#First *fork* your copy. Then copy-paste your code below this line 👇
-#Finally click "Run" to execute the tests
-
 def prime_checker(number):
-  if number>1:  
-    for i in range(2, number):
-      if number%i == 0:
-        print(f"{number} is not a prime number.")
-        break
 
+  try:
+    number = float(number)
+
+    if number == int(number):
+      number = math.trunc(number)
+      if number > 1:
+        for i in range(2, number):
+          if number % i == 0:
+            print("It's not a prime number.")
+            break
+        else :
+          print("It's a prime number.")
+      elif number < 0 :
+        print("You've entered a negative number.")
+      elif number==0 or 1:
+        print(f'{number} is not a prime number.')
     else :
-      print(f"{number} is a prime number")
-  elif number==0 or 1 :
-    print(f"{number} is not a prime number.")
-  else:
-    print("You've entered negative number")
+      print("Yo've entered a float type number.")
+
+  except :
+     print("You've entered not a number")
+    
+
 
 
 #Write your code above this line 👆
     
 #Do NOT change any of the code below👇
-n = int(input("Check this number: "))
+n = input("Check this number: ")
 prime_checker(number=n)
+
+
+
